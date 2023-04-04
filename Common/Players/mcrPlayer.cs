5865ef478ef0e4dfbdf3515313505506beb68e3f
RaphaelDaubelcour <daubelcour63@gmail.com>
2023-04-03 21:08:33 -0400

    Milk function call in mcrPlayer instead of mcrAccessoriesPlayer

---
+++ Common/Players/mcrPlayer.cs
@@ -0,0 +1,19 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+
+namespace mcraria.Common.Players
+{
+    public class mcrPlayer : ModPlayer
+    {
+        public override void PostNurseHeal(NPC nurse, int health, bool removeDebuffs, int price)
+        {
+            if (Main.rand.Next(0, 100) == 1)
+            {
+                Player.QuickSpawnItem(NPC.GetSource_NaturalSpawn(),
+                    ModContent.ItemType<Content.Items.Consumables.Buckets.MilkBucket>());
+            }
+            base.PostNurseHeal(nurse, health, removeDebuffs, price);
+        }
+    }
+}
+
